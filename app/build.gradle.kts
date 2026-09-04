6d274b86eda7810545b02696971545be888fe451
estiaksoyeb <estiakahamed122@gmail.com>
2026-09-04 17:34:09 +0600

    chore(release): bump version to 5.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "xyz.mpv.rex"
     minSdk = 26
     targetSdk = 36
-    versionCode = 211
-    versionName = "5.0.0"
+    versionCode = 212
+    versionName = "5.1.0"
 
     vectorDrawables {
       useSupportLibrary = true
